e3f1c35b3a5de391c17c5635831a4b6cddb58c90
David Vedenichev <davidvedenichev1@gmail.com>
2024-06-23 01:26:36 +0500

    Changed title

--- Button.py
+++ Button.py
@@ -6,18 +6,20 @@ class Button:
                  centery: float, 
                  width: float, 
                  height: float, 
-                 image_path: str,
-                 active_image_path: str):
+                 image_path: str):
         self.rect = pg.Rect(0, 0, width, height)
         self.rect.center = centerx, centery
+        
         try:
             self.image = pg.transform.scale(pg.image.load(image_path), (width, height)).convert_alpha()
         except:
             self.image = pg.transform.scale(pg.image.load('assets/error.png'), (width, height)).convert_alpha()
+
         try:
-            self.active_image = pg.transform.scale(pg.image.load(active_image_path), (width, height)).convert_alpha()
+            self.active_image = pg.transform.scale(pg.image.load(image_path.replace('.png', '_active.png')), (width, height)).convert_alpha()
         except:
             self.active_image = pg.transform.scale(pg.image.load('assets/error.png'), (width, height)).convert_alpha()
+        
         self.is_clicked = False
         self.is_collide = False
 
--- Menu.py
+++ Menu.py
@@ -1,27 +1,33 @@
 import pygame as pg
 from Button import Button
+import time
+import math
 
 class Menu:
     def __init__(self, screen: pg.Surface, settings: dict):
+        buttons_size = (516/settings["GUI_SIZE"], 116/settings["GUI_SIZE"])
+
         self.play_button = Button(screen.get_width()/2, screen.get_height()/10*4.5, 
-                                  516/settings["GUI_SIZE"], 116/settings["GUI_SIZE"], 
-                                  "assets/play_button.png", "assets/play_button_active.png")
-        self.settings_button = Button(screen.get_width()/2, screen.get_height()/10*4.5+116/settings["GUI_SIZE"], 
-                                      516/settings["GUI_SIZE"], 116/settings["GUI_SIZE"], 
-                                      "assets/settings_button.png", "assets/settings_button_active.png")
-        self.quit_button = Button(screen.get_width()/2, screen.get_height()/10*4.5+116/settings["GUI_SIZE"]*2, 
-                                  516/settings["GUI_SIZE"], 116/settings["GUI_SIZE"], 
-                                  "assets/quit_button.png", "assets/quit_button_active.png")
+                                  *buttons_size, "assets/play_button.png")
+        
+        self.settings_button = Button(screen.get_width()/2, screen.get_height()/10*4.5+buttons_size[1], 
+                                      *buttons_size, "assets/settings_button.png")
+        
+        self.quit_button = Button(screen.get_width()/2, screen.get_height()/10*4.5+buttons_size[1]*2, 
+                                 *buttons_size, "assets/quit_button.png")
+        
         self.title = pg.transform.scale(pg.image.load('assets/title.png'), 
-                                        (879/settings["GUI_SIZE"], 118/settings["GUI_SIZE"])
-                                        ).convert_alpha()
-        self.title_rect = pg.Rect(0, 0, 879/settings["GUI_SIZE"], 118/settings["GUI_SIZE"])
+                                        (715/settings["GUI_SIZE"], 118/settings["GUI_SIZE"])).convert_alpha()
+        
+        self.title_rect = pg.Rect(0, 0, *self.title.get_size())
         self.title_rect.center = screen.get_width()/2, screen.get_height()/10*2
+
         self.gradient = pg.transform.scale(pg.image.load('assets/gradient.png'), 
                                         (settings["WIDTH"], 394/1080*settings["HEIGHT"])
                                         ).convert_alpha()
-        self.gradient_rect = pg.Rect(0, settings["HEIGHT"]-394/1080*settings["HEIGHT"], 
-                                  settings["WIDTH"], 394/1080*settings["HEIGHT"])
+        
+        self.gradient_rect = pg.Rect(0, settings["HEIGHT"]-self.gradient.get_size()[1], 
+                                  *self.gradient.get_size())
 
     def update(self, screen: pg.Surface) -> str:
         feedback = ''
@@ -42,7 +48,12 @@ class Menu:
     def draw(self, screen: pg.Surface):
         screen.fill("#606060")
         screen.blit(self.gradient, self.gradient_rect)
-        screen.blit(self.title, self.title_rect)
+
+        title_rotated_image = pg.transform.rotate(self.title, math.sin(time.time()%360*2)*5)
+        title_rotated_image_rect = pg.Rect(0, 0, *title_rotated_image.get_size())
+        title_rotated_image_rect.center = self.title_rect.center
+        screen.blit(title_rotated_image, title_rotated_image_rect)
+
         self.play_button.draw(screen)
         self.settings_button.draw(screen)
         self.quit_button.draw(screen)
